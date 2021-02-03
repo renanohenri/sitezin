1327f75636aec6dda4ad8417f40d607e0556981c
Eduardo Henrique Celestino Oliveira <eduardo98riqui@gmail.com>
2021-02-03 20:44:16 -0300

    refatoração

--- js/index.js
+++ js/index.js
@@ -13,18 +13,41 @@ if ($(window).width() <= 1200)
 
 
 
-
 $("#btn-menu").click(function () {
-    if ($('#btn-menu').attr('aria-expanded') === 'true') {
-        console.log('entre aqui')
-        $('#slider').css({ display: 'block', height: '60%' })
-        $('#titulo').css({ padding: '0' })
-    } else {
-        console.log('to aqui')
-        $('#slider').css({ display: 'flex', height: '85%' })
-    }
-
+    // if ($('#btn-menu').attr('aria-expanded') === 'true') {
+    //     $('#slider .row').css({ 'margin-top': '50%' })
+    // } else {
+    //     // console.log('to aqui')
+    //     $('#slider .row').css({ 'margin-top': '0' })
+    // }
 });
+
+var lastScrollTop = 0;
+$(window).scroll(function () {
+    var st = $(this).scrollTop();
+    var header = $('#header');
+    var header_alt = $('#header-alt');
+    setTimeout(function () {
+        if (st == 0) {
+            header.css({ 'visibility': 'visible' })
+            header_alt.css({ 'visibility': 'hidden' })
+        }
+        else if (st > lastScrollTop) {
+            header.css({ 'visibility': 'hidden' })
+            header_alt.css({ 'visibility': 'hidden', 'opacity': '0' })
+        } else {
+            header_alt.css({ 'visibility': 'visible', 'opacity': '1' })
+        }
+        lastScrollTop = st;
+    }, 100);
+});
+
+$("#imagem").attr('src', 'assets/img/logo-rezende-neiva-advogados-color.svg');
+
+
+
+
+
 
 
 
